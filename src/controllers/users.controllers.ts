0fd8bf3ae26a9434ea7d7fd58846c2aea8a42ef4
khoanc2304 <1fvhtkhoa@gmail.com>
2026-01-21 11:24:06 +0700

    Config project

---
+++ src/controllers/users.controllers.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express'
+
+export const loginController = (req: Request, res: Response) => {
+  const { email, password } = req.body
+
+  if (email === 'khoa@gmail.com' && password === '123456') {
+    return res.status(200).json({
+      message: 'Login success'
+    })
+  }
+
+  return res.status(400).json({
+    error: 'Login failed'
+  })
+}
